919535a1ad50255c4ef625ba24aff18a6fea6f3b
Christophe Foyer <christophe.aa.foyer@gmail.com>
2016-07-06 18:29:43 +0200

    update
    First "working" version! It doesn't analyse past data but just the day's data (which explains the extremely low accuracy)

--- Naive Bayes Classifier.py
+++ Naive Bayes Classifier.py
@@ -25,74 +25,74 @@ def importExcel(filename):
     for sheet in excel.sheets():
         number_of_rows = sheet.nrows
         number_of_columns = sheet.ncols
-        dataset = [['unknown'] * (number_of_columns + 3)] * number_of_rows
+        dataset = [[0.0 for x in range(number_of_columns + 3)] for y in range(number_of_rows)]
         date = []
         date_string = []
         price = []
         rows = []
-        for row in range(1, number_of_rows):
-            #excel stores dates as the number of days since 1900-Jan-0 (not sure if that means january 1st or december 31st but that won't matter much in our case)
-            #new method: substract number of days in year until negative
-            date_string  = str(sheet.cell(row,0).value)
-            days = float(date_string)
-            dataset[row][0] = int(days)
-            i = 0
-            leap = 0
-            #this will find how many years and how many leftover days for that year
-            while days >= (365 + leap):
+        for row in range(1, number_of_rows-1):
+                #excel stores dates as the number of days since 1900-Jan-0 (not sure if that means january 1st or december 31st but that won't matter much in our case)
+                #new method: substract number of days in year until negative
+                date_string  = str(sheet.cell(row,0).value)
+                days = float(date_string)
+                dataset[row-1][0] = float(days)
+                i = 0
                 leap = 0
-                if i + 1900 in leap_years:
-                    leap = 1
-                days = days - 365 - leap
-                i = i + 1
-            year = i
-            #now find the month and leftover days given leftover days
-            month = 1
-            for i in range(1, 12):
-                #for debugging
-                #if year + 1900 == 1998:
-                #    print days, month ,year
-                ##############
-                if (year + 1900 in leap_years) and (i == 2):
-                     leap = 1
+                #this will find how many years and how many leftover days for that year
+                while days >= (365 + leap):
+                    leap = 0
+                    if i + 1900 in leap_years:
+                        leap = 1
+                    days = days - 365 - leap
+                    i = i + 1
+                year = i
+                #now find the month and leftover days given leftover days
+                month = 1
+                for i in range(1, 12):
+                    #for debugging
+                    #if year + 1900 == 1998:
+                    #    print days, month ,year
+                    ##############
+                    if (year + 1900 in leap_years) and (i == 2):
+                         leap = 1
+                    else:
+                         leap = 0
+                    if days <= (month_day_count[i-1] + leap):
+                        break
+                    else:
+                        days = days - month_day_count[i-1] - leap
+                        month = i + 1
+                #now we should have the exact date seperated in day, month and year
+                if (year + 1900 in leap_years):
+                         leap = 1
                 else:
-                     leap = 0
-                if days <= (month_day_count[i-1] + leap):
-                    break
-                else:
-                    days = days - month_day_count[i-1] - leap
-                    month = i + 1
-            #now we should have the exact date seperated in day, month and year
-            if (year + 1900 in leap_years):
-                     leap = 1
-            else:
-                     leap = 0
-            #different format, easier to use decimals
-            #date_new = int(days), int(month), int(year+1900),
-            date_new = (year + 1900) + month / 12 + days /(365 + leap)
-            date.append(date_new)
-            dataset[row][1] = year + 1900
-            dataset[row][2] = month
-            dataset[row][3] = int(days)
-                
-            value  = (sheet.cell(row,1).value)
-            try:
-                value = str(int(value))
-            except ValueError:
-                pass
-            finally:
-                dataset[row][4] = float(value)
-                
-            #now the rest of the data
-            for col in range(2, number_of_columns):
-                value  = (sheet.cell(row,col).value)
+                         leap = 0
+                #different format, easier to use decimals
+                #date_new = int(days), int(month), int(year+1900),
+                date_new = (year + 1900) + month / 12 + days /(365 + leap)
+                date.append(date_new)
+                dataset[row-1][1] = float(year + 1900)
+                dataset[row-1][2] = float(month)
+                dataset[row-1][3] = float(days)
+                    
+                value  = (sheet.cell(row,1).value)
                 try:
-                    value = float(value)
+                    value = str(int(value))
                 except ValueError:
                     pass
                 finally:
-                    dataset[row][col + 3] = value
-                    #now all the data should be accessible from the "dataset" array
+                    dataset[row-1][4] = float(value)
+                    
+                #now the rest of the data
+                for col in range(2, number_of_columns):
+                    value  = (sheet.cell(row,col).value)
+                    try:
+                        dataset[row-1][col + 3] = float(value)
+                        value = float(value)
+                    except ValueError:
+                        pass
+                #now all the data should be accessible from the "dataset" array
+    print dataset
     return dataset
 
 def splitDataset(dataset, splitRatio):
@@ -108,18 +108,21 @@ def separateByClass(dataset):
 	separated = {}
 	for i in range(len(dataset)):
 		vector = dataset[i]
-		if (round(vector[4]) not in separated):
-			separated[round(vector[4])] = []
-		separated[round(vector[4])].append(vector)
+		if (vector[4] not in separated):
+			separated[vector[4]] = []
+		separated[vector[4]].append(vector)
 	return separated
 
 def mean(numbers):
 	return sum(numbers)/float(len(numbers))
 
 def stdev(numbers):
-	avg = mean(numbers)
-	variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
-	return math.sqrt(variance)
+        if len(numbers) > 1:
+            avg = mean(numbers)
+            variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+            return math.sqrt(variance)
+        else:
+            return 0
 
 def summarize(dataset):
 	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
@@ -134,8 +137,11 @@ def summarizeByClass(dataset):
 	return summaries
 
 def calculateProbability(x, mean, stdev):
-	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+        if stdev !=0:
+            exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+            return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+        else:
+            return 1
 
 def calculateClassProbabilities(summaries, inputVector):
 	probabilities = {}
@@ -143,9 +149,6 @@ def calculateClassProbabilities(summaries, inputVector):
 		probabilities[classValue] = 1
 		for i in range(len(classSummaries)):
 			mean, stdev = classSummaries[i]
-                        #fixing 0 standard deviation
-			if stdev == 0:
-				stdev = 10^-10
 			x = inputVector[i]
 			probabilities[classValue] *= calculateProbability(x, mean, stdev)
 	return probabilities
@@ -180,7 +183,6 @@ def relativeData(dataset):
 def main():
 	splitRatio = 0.67
 	dataset = importExcel(filename)
-	print dataset
 	#reorganise data to include past days
 	#allData = relativeData(dataset)
 	print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
